a1b6bfe1b0db6f31b5c088b318dc52053e5b2366
purva <purvashinde06062001@gmail.com>
2026-06-25 21:14:53 +0530

    Initial project commit

---
+++ src/test/java/GitPractice/LoginTest.java
@@ -0,0 +1,31 @@
+package GitPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void loginTest() throws InterruptedException {
+
+
+	WebDriver driver = new EdgeDriver();
+
+
+	driver.get("https://www.saucedemo.com/");
+	driver.manage().window().maximize();
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	driver.findElement(By.id("login-button")).click();
+	
+	Thread.sleep(3000);
+	driver.quit();
+
+
+	}
+
+
+
+}
---
+++ src/test/java/GitPractice/WebDriverUtility.java
@@ -0,0 +1,18 @@
+package GitPractice;
+
+
+import org.openqa.selenium.WebDriver;
+
+
+public class WebDriverUtility {
+
+
+public void maximize(WebDriver driver)
+{
+
+driver.manage().window().maximize();
+
+}
+
+
+}
